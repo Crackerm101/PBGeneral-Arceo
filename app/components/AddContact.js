55c76a540f6fae5b6a455aeb80227bd32fff619e
Jayvee-Friday <91900181+Crackerm101@users.noreply.github.com>
2026-05-22 23:25:36 +0800

    Update AddContact.js

--- app/components/AddContact.js
+++ app/components/AddContact.js
@@ -3,12 +3,12 @@
 import React from 'react';
 import { MdCloudDownload } from "react-icons/md";
 const contact = {
-  firstName: 'PBGEN GLENN OLIVER C',
-  lastName: 'CINCO ',
-  phone: '+639173536887',
-  email: 'cincoglennoliver@yahoo.com',
-  organization: 'District Director,SPD',
-  location: 'Fort Bonifacio,Taguig City 1100, Philippines',
+ firstName: 'PBGEN RANDY Y',
+  lastName: 'ARCEO ',
+  phone: '+639175741332',
+  email: 'ryrcu1994@yahoo.com',
+  organization: 'Philippine National Police (PNP)',
+  location: 'Taguig City 1100, Philippines',
 };
 
 function generateVCF(contact) {
